941398a62b7a35839adeb43c61bdf27b2b01e30a
ninizinzu <nirmit.zinzuwadia@mail.utoronto.ca>
2021-01-31 13:16:38 -0500

    Creates a login page

--- app/src/main/java/me/nirmit/ready/MainActivity.java
+++ app/src/main/java/me/nirmit/ready/MainActivity.java
@@ -2,13 +2,56 @@ package me.nirmit.ready;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+
+    private Context mContext;
+    private Button btnLogin;
+    private Button btnSignup;
+    private EditText mEmail, mPassword;
+    private ProgressBar mProgressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mContext = MainActivity.this;
+        btnLogin = (Button) findViewById(R.id.btn_login);
+        btnSignup = (Button) findViewById(R.id.btn_signup);
+        mProgressBar = (ProgressBar) findViewById(R.id.progressBar);
+        mEmail = (EditText) findViewById(R.id.input_email);
+        mPassword = (EditText) findViewById(R.id.input_password);
+
+        mProgressBar.setVisibility(View.GONE);
+        signupBtnLogic();
+        loginBtnLogic();
     }
+
+    private void signupBtnLogic() {
+        btnSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // [TODO] add logic here
+            }
+        });
+    }
+
+    private void loginBtnLogic() {
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // [TODO] add logic here
+            }
+        });
+    }
+
 }
