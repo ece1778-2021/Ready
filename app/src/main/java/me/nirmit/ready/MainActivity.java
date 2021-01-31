27e6f653ab8ab2855d47bae55ee4196b2287fe93
ninizinzu <nirmit.zinzuwadia@mail.utoronto.ca>
2021-01-31 12:23:05 -0500

    Initial commit. Project setup.

---
+++ app/src/main/java/me/nirmit/ready/MainActivity.java
@@ -0,0 +1,14 @@
+package me.nirmit.ready;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
