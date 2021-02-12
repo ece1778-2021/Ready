b1a413dc5855bcfc4df9a05a763e79cef9d6a090
ninizinzu <nirmit.zinzuwadia@mail.utoronto.ca>
2021-02-12 18:03:37 -0500

    Teacher and Student Modes added. To be deleted ones all the activities are completed.

--- app/src/main/java/me/nirmit/ready/Login/MainActivity.java
+++ app/src/main/java/me/nirmit/ready/Login/MainActivity.java
@@ -1,13 +1,18 @@
-package me.nirmit.ready;
+package me.nirmit.ready.Login;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+
+import me.nirmit.ready.R;
+import me.nirmit.ready.Student.StudentCourses;
+import me.nirmit.ready.Teacher.TeacherQuizes;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,6 +23,7 @@ public class MainActivity extends AppCompatActivity {
     private Button btnSignup;
     private EditText mEmail, mPassword;
     private ProgressBar mProgressBar;
+    private Button tempTeacher, tempStudent; // TODO: delete
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,10 +36,14 @@ public class MainActivity extends AppCompatActivity {
         mProgressBar = (ProgressBar) findViewById(R.id.progressBar);
         mEmail = (EditText) findViewById(R.id.input_email);
         mPassword = (EditText) findViewById(R.id.input_password);
+        //TODO: delete the following
+        tempTeacher = (Button) findViewById(R.id.teacherBtn);
+        tempStudent = (Button) findViewById(R.id.studentBtn);
 
         mProgressBar.setVisibility(View.GONE);
         signupBtnLogic();
         loginBtnLogic();
+        tempBtnLogic();
     }
 
     private void signupBtnLogic() {
@@ -50,6 +60,25 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 // [TODO] add logic here
+            }
+        });
+    }
+
+    // TODO: delete ones layouts completed
+    private void tempBtnLogic() {
+        tempTeacher.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(mContext, TeacherQuizes.class);
+                startActivity(intent);
+            }
+        });
+
+        tempStudent.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(mContext, StudentCourses.class);
+                startActivity(intent);
             }
         });
     }
---
+++ app/src/main/java/me/nirmit/ready/Teacher/TeacherQuizes.java
@@ -0,0 +1,17 @@
+package me.nirmit.ready.Teacher;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import me.nirmit.ready.R;
+
+public class TeacherQuizes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.teacher_add_quiz);
+    }
+}
