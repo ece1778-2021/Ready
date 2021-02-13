1b3e9ee921a6cba2cfedc56f6dd488788725bc93
ninizinzu <nirmit.zinzuwadia@mail.utoronto.ca>
2021-02-13 16:08:00 -0500

    Teacher view: add quizzes layout

---
+++ app/src/main/java/me/nirmit/ready/Teacher/QuizAdapter.java
@@ -0,0 +1,89 @@
+package me.nirmit.ready.Teacher;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Adapter;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+import me.nirmit.ready.R;
+
+public class QuizAdapter extends RecyclerView.Adapter<QuizAdapter.ViewHolder> {
+
+    private LayoutInflater layoutInflater;
+    private List<String> quizzes;
+
+    QuizAdapter(Context context, List<String> quizzes) {
+        this.layoutInflater = LayoutInflater.from(context);
+        this.quizzes = quizzes;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = layoutInflater.inflate(R.layout.teacher_custom_quiz_view, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+        // bind the textview with data received
+        String quiz = quizzes.get(position);
+        holder.quizName.setText(quiz);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return quizzes.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+
+        TextView quizName;
+        Button btnDelete, btnPublish;
+        CardView quizCard;
+
+        public ViewHolder(@NonNull final View itemView) {
+            super(itemView);
+            quizName = itemView.findViewById(R.id.quizName);
+            btnDelete = itemView.findViewById(R.id.btnDelete);
+            btnPublish = itemView.findViewById(R.id.btnPublish);
+            quizCard = itemView.findViewById(R.id.quizCard);
+
+            quizCard.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Log.d("QUIZ CARD:", "Card for: " + quizName.getText());
+                    Toast.makeText(itemView.getContext(), "hehehe", Toast.LENGTH_LONG).show();
+                }
+            });
+
+            btnPublish.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Log.d("QUIZ CARD:", "Publishing Card for: " + quizName.getText());
+                }
+            });
+
+            btnDelete.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Log.d("QUIZ CARD:", "Deleting Card for: " + quizName.getText());
+                }
+            });
+        }
+    }
+
+}
--- app/src/main/java/me/nirmit/ready/Teacher/TeacherAddQuizActivity.java
+++ app/src/main/java/me/nirmit/ready/Teacher/TeacherAddQuizActivity.java
@@ -1,17 +1,82 @@
 package me.nirmit.ready.Teacher;
 
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toolbar;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 import me.nirmit.ready.R;
 
 public class TeacherAddQuizActivity extends AppCompatActivity {
 
+    RecyclerView recyclerView;
+    QuizAdapter quizAdapter;
+    ArrayList<String> quizNames;
+    private TextView topBarTitle;
+    private Button btnAddQuiz;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_teacher_add_quiz);
+
+        topBarTitle = (TextView) findViewById(R.id.topBarTitle);
+        topBarTitle.setText("Assessments");
+        btnAddQuiz = (Button) findViewById(R.id.btnAddQuiz);
+
+        quizNames = new ArrayList<>();
+        quizNames.add("Test 1");
+        quizNames.add("Test 2");
+        quizNames.add("Test 3");
+        quizNames.add("Test 4");
+        quizNames.add("Test 5");
+        quizNames.add("Test 6");
+        quizNames.add("Test 7");
+
+        recyclerView = findViewById(R.id.rcvQuizzes);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        quizAdapter = new QuizAdapter(this,  quizNames);
+        recyclerView.setAdapter(quizAdapter);
+
+        btnAddQuizLogic();
+    }
+
+    private void btnAddQuizLogic() {
+        btnAddQuiz.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showAddQuizDialog(TeacherAddQuizActivity.this);
+            }
+        });
+    }
+
+    private void showAddQuizDialog(Context c) {
+        final EditText taskEditText = new EditText(c);
+        AlertDialog dialog = new AlertDialog.Builder(c)
+                .setTitle("Add an Assessment")
+                .setMessage("Assessment Name: ")
+                .setView(taskEditText)
+                .setPositiveButton("Done", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String task = String.valueOf(taskEditText.getText());
+                        quizNames.add(task);
+                    }
+                })
+                .setNegativeButton("Cancel", null)
+                .create();
+        dialog.show();
     }
 }
