28c6e51976c7531a95971e39fcc39e7891b38cb7
ninizinzu <nirmit.zinzuwadia@mail.utoronto.ca>
2021-02-20 13:17:58 -0500

    rearranged registration page

--- app/src/main/java/me/nirmit/ready/Login/RegisterActivity.java
+++ app/src/main/java/me/nirmit/ready/Login/RegisterActivity.java
@@ -1,16 +1,12 @@
 package me.nirmit.ready.Login;
 
-import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.Bitmap;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
@@ -26,8 +22,7 @@ import me.nirmit.ready.Teacher.TeacherAddQuizActivity;
 
 public class RegisterActivity extends AppCompatActivity {
 
-    private static final String LOG = "RegisterActivity";
-    private static final int REQUEST_IMAGE_CAPTURE = 1;
+    private static final String TAG = "RegisterActivity";
 
     private Context mContext;
     private TextView topBarTitle;
@@ -35,14 +30,11 @@ public class RegisterActivity extends AppCompatActivity {
     private EditText password;
     private EditText conPassword;
     private EditText name;
-    private EditText studentID;
+    private EditText ID;
     private EditText phone;
-    private CircleImageView pic;
     private Button signUp;
     private RadioGroup radioGroup;
     private RadioButton radioButton;
-    private LinearLayout linearLayout;
-    private FloatingActionButton fab;
 
 
     @Override
@@ -54,19 +46,14 @@ public class RegisterActivity extends AppCompatActivity {
         topBarTitle.setText("Register");
 
         mContext = RegisterActivity.this;
-        linearLayout = findViewById(R.id.student_content);
         email = findViewById(R.id.email);
         password = findViewById(R.id.password);
         conPassword = findViewById(R.id.pw2);
         name = findViewById(R.id.name);
-        studentID = findViewById(R.id.id);
+        ID = findViewById(R.id.id);
         phone = findViewById(R.id.phone);
-        pic = findViewById(R.id.profile_pic);
         signUp = findViewById(R.id.signup_button);
         radioGroup = findViewById(R.id.radio_group);
-        fab = findViewById(R.id.profile_fab);
-
-        takePicLogic();
         signUpLogic();
 
     }
@@ -81,45 +68,10 @@ public class RegisterActivity extends AppCompatActivity {
         return valid;
     }
 
-
     public void onRadioButtonClicked(View view) {
         int selectedId = radioGroup.getCheckedRadioButtonId();
         radioButton = (RadioButton) findViewById(selectedId);
-        if (radioButton.getText().toString().equals("Student")) {
-            linearLayout.setVisibility(View.VISIBLE);
-        }
-        else {
-            linearLayout.setVisibility(View.GONE);
-        }
-        Log.d(LOG, radioButton.getText().toString());
-    }
-
-    private void takePicLogic() {
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                dispatchTakePictureIntent();
-            }
-        });
-    }
-
-    // Launch Camera Function
-    private void dispatchTakePictureIntent() {
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        try {
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-        } catch (ActivityNotFoundException e) {
-            Log.d(LOG, "Error: Unable to take picture");
-        }
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            Bundle extras = data.getExtras();
-            Bitmap imageBitmap = (Bitmap) extras.get("data");
-        }
+        Log.d(TAG, radioButton.getText().toString());
     }
 
     private void signUpLogic() {
@@ -137,13 +89,9 @@ public class RegisterActivity extends AppCompatActivity {
                         startActivity(intent);
                     }
                 }
-                else Log.d(LOG, "invalid user info");
+                else Log.d(TAG, "invalid user info");
             }
         });
     }
-
-
-
-
 
 }
